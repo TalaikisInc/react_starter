f60546baf96519caf9128d489fde7c9c985c7bb8
Tadas Talaikis <xenu256@users.noreply.github.com>
2018-09-10 00:33:47 +0300

    rms

--- api/README.md
+++
@@ -1,9 +0,0 @@
-# React Starter API
-
-## Run
-
-```bash
-npm i
-npm install -g prisma
-prisma deploy
-```
--- api/env.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({ path: process.env.NODE_ENV === 'production' ? '.env' : '.env.development' })
-
-module.exports = {
-  siteTitle: 'Sample Site',
-  PRISMA_ENDPOINT: process.env.PRISMA_ENDPOINT,
-  APP_SECRET: process.env.APP_SECRET
-}
